03b6d79358479b0d276a6cd6a28d76245fb7eac7
ishumski <whalterwhite23@gmail.com>
2020-11-28 19:05:22 +0300

    Delete comments

--- lesson3_Tasks/js/task_3.js
+++ lesson3_Tasks/js/task_3.js
@@ -1,35 +1,35 @@
 /*3. Написать функцию compact, которая принимает в качестве параметров массив, 
 и возвращает новый, в котором нет значений false, undefined, пустых строк, нулей и null.*/
-// const array = [1, false, 2, undefined, " ", 3, 4, 0, null, 5];
+const array = [1, false, 2, undefined, " ", 3, 4, 0, null, 5];
 
-// function compact(arr) {
+function compact(arr) {
 
-//     let newArr = [];
+    let newArr = [];
 
-//     for (let i = 0; i < arr.length; i++) {
-//         if (!Array.isArray(arr)) {
-//             return "Please, enter array!";
-//         }
-//         if (typeof arr[i] != "boolean" &&
-//             typeof arr[i] != "undefined" &&
-//             arr[i] != 0 &&
-//             arr[i] != " " &&
-//             arr[i] != null) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!Array.isArray(arr)) {
+            return "Please, enter array!";
+        }
+        if (typeof arr[i] != "boolean" &&
+            typeof arr[i] != "undefined" &&
+            arr[i] != 0 &&
+            arr[i] != " " &&
+            arr[i] != null) {
 
-//             newArr.push(arr[i]);
-//         }
-//     }
-//     return newArr;
-// }
-// console.log(compact(array));
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+console.log(compact(array));
 
 /********************************************************************/
-// const array = [1, false, 2, undefined, " ", 3, 4, 0, null, 5];
+const array2 = [1, false, 2, undefined, " ", 3, 4, 0, null, 5];
 
-// function compact(arr) {
-//     if (!Array.isArray(arr)) {
-//         return "Please, enter array!";
-//     }
-//     return arr.filter(item => typeof item != "boolean" && typeof item != "undefined" && item != 0 && item != " " && item != null);
-// }
-// console.log(compact(array));
+function compact(arr2) {
+    if (!Array.isArray(arr2)) {
+        return "Please, enter array!";
+    }
+    return arr2.filter(item => typeof item != "boolean" && typeof item != "undefined" && item != 0 && item != " " && item != null);
+}
+console.log(compact(array2));
--- lesson3_Tasks/js/task_5.js
+++ lesson3_Tasks/js/task_5.js
@@ -2,31 +2,31 @@
 и возвращает новый, который состоит из общих элементов переданных массивов,
 то есть тех, которые есть и в первом, и во втором.*/
 
-// const firstArr = [4, undefined, 15, null, 23, 42];
-// const secondArr = ["mama mia!", 13, true, 98];
+const firstArr = [4, undefined, 15, null, 23, 42];
+const secondArr = ["mama mia!", 13, true, 98];
 
-// function intersection(arr1, arr2) {
+function intersection(arr1, arr2) {
 
-//     if (!Array.isArray(arr1) && !Array.isArray(arr2)) {
-//         return ("Please, enter arrays");
-//     }
+    if (!Array.isArray(arr1) && !Array.isArray(arr2)) {
+        return ("Please, enter arrays");
+    }
 
-//     return arr1.concat(arr2);
-// }
-// console.log(intersection(firstArr, secondArr));
+    return arr1.concat(arr2);
+}
+console.log(intersection(firstArr, secondArr));
 
 /***********************************************************/
-// let firstArr = [4, undefined, 15, null, 23, 42];
-// let secondArr = ["mama mia!", 13, true, 98];
+let firstArr = [4, undefined, 15, null, 23, 42];
+let secondArr = ["mama mia!", 13, true, 98];
 
-// function mergingOfArr(arr1, arr2) {
-//     if (!Array.isArray(arr1) && !Array.isArray(arr2)) {
-//         return ("Please, enter arrays");
-//     }
-//     for (let i = 0; i < arr2.length; i++) {
-//         arr1.push(arr2[i]);
-//     }
+function mergingOfArr(arr1, arr2) {
+    if (!Array.isArray(arr1) && !Array.isArray(arr2)) {
+        return ("Please, enter arrays");
+    }
+    for (let i = 0; i < arr2.length; i++) {
+        arr1.push(arr2[i]);
+    }
 
-//     return arr1;
-// }
-// console.log(mergingOfArr(firstArr, secondArr));
+    return arr1;
+}
+console.log(mergingOfArr(firstArr, secondArr));
